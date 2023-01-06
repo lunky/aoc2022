91ac083cedc6f87a7fafdf48faba5fcb1d77c224
Quinn Wilson <github@midworld.org>
2023-01-06 15:25:24 -0600

    finally accept hlint suggestion

--- src/AocLib.hs
+++ src/AocLib.hs
@@ -8,7 +8,7 @@ import Data.List (foldl')
 grid :: [[a]] -> [((Int,Int), a)]
 grid = concat . zipWith (curry grid') [1..]
   where
-    grid' (y,xs) = map (\(x,datum)-> ((x,y),datum)) $ zip [1..] xs
+    grid' (y,xs) = zipWith ( \ x datum -> ((x, y), datum)) [1 .. ] xs
 
 adjacent :: (Int,Int) -> [(Int,Int)]
 adjacent (x,y) = [(x,y-1),(x,y+1),(x-1,y),(x+1,y)]
