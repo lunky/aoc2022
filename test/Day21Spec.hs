2a320a4732ac51774b94358f9e1fd5f2de097e2c
lunky <quinn@midworld.org>
2023-02-03 14:28:16 -0600

    Day21 b done

--- test/Day21Spec.hs
+++ test/Day21Spec.hs
@@ -6,10 +6,10 @@ import Test.Hspec
 spec :: Spec
 spec = do
   describe "Day21" $ do
-    xit "should do sample 1" $ do
+    it "should do sample 1" $ do
       let expected = 152
       day21 _input `shouldBe` expected
   describe "Day21b" $ do
-    xit "should do sample 1" $ do
-      let expected = 0
+    it "should do sample 1" $ do
+      let expected = 301
       day21b _input `shouldBe` expected
